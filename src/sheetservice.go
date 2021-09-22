74caf94c8bc1f4b7dd6866dfcd6a2086028a672a
Aaron Traas <aaron@traas.org>
2021-09-22 14:28:45 -0400

    grabs and formats configurable ranges

--- src/sheetservice.go
+++ src/sheetservice.go
@@ -15,17 +15,45 @@ type ApiConfig struct {
 	ApiKey string `json:"apiKey"`
 }
 
+type AttributeRow struct {
+	Name  string `json:"name"`
+	Range string `json:"range"`
+}
+
+type ConfigEntry struct {
+	CharacterKey string         `json:"characterKey"`
+	SheetId      string         `json:"sheetId"`
+	Attributes   []AttributeRow `json:"attributes"`
+}
+
+func getConfig() []ConfigEntry {
+
+	fileBytes, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		log.Fatalf("Unable to read config file: %v", err)
+	}
+
+	var config []ConfigEntry
+
+	err = json.Unmarshal([]byte(fileBytes), &config)
+	if err != nil {
+		log.Fatalf("Invalid config.json: %v", err)
+	}
+
+	return config
+}
+
 func getService() *sheets.Service {
 	ctx := context.Background()
 
-	apiKeyFile, err := ioutil.ReadFile("api-key.json")
+	fileBytes, err := ioutil.ReadFile("api-key.json")
 	if err != nil {
 		log.Fatalf("Unable to read API config file: %v", err)
 	}
 
 	var apiConfig ApiConfig
 
-	err = json.Unmarshal([]byte(apiKeyFile), &apiConfig)
+	err = json.Unmarshal([]byte(fileBytes), &apiConfig)
 	if err != nil {
 		log.Fatalf("Invalid api-key.json: %v", err)
 	}
@@ -39,20 +67,42 @@ func getService() *sheets.Service {
 }
 
 func main() {
+	config := getConfig()
 	srv := getService()
 
-	spreadsheetId := "19J5G8y9jKLAYc7xkMwaRj0eGuykzS46mrV4dOFCvYRE"
-	readRange := "HP_CURRENT"
-	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+	// map for serialization of results
+	out := map[string]interface{}{}
+
+	for _, c := range config {
+		// Construct array of ranges to call from sheet in batch
+		ranges := []string{}
+		for _, attr := range c.Attributes {
+			ranges = append(ranges, attr.Range)
+		}
+
+		// Query sheet for list of ranges
+		fmt.Printf("---\nRetrieving attributes for '%s'... ", c.CharacterKey)
+		batchResp, err := srv.Spreadsheets.Values.BatchGet(c.SheetId).Ranges(ranges...).Do()
+		if err != nil {
+			log.Fatalf("Unable to retrieve data from sheet: %v", err)
+		} else {
+			fmt.Println("Success!")
+		}
+
+		// map ranges to names from config attributes
+		entry := map[string]interface{}{}
+		for i, attr := range c.Attributes {
+			valueRange := batchResp.ValueRanges[i]
+			if len(valueRange.Values) == 0 {
+				fmt.Println("No data found.")
+			} else {
+				entry[attr.Name] = valueRange.Values[0][0]
+			}
+		}
+
+		out[c.CharacterKey] = entry
 	}
 
-	if len(resp.Values) == 0 {
-		fmt.Println("No data found.")
-	} else {
-		for _, row := range resp.Values {
-			fmt.Printf("Current HP: %s\n", row[0])
-		}
-	}
+	jsonString, _ := json.MarshalIndent(out, "", "  ")
+	fmt.Printf("\n%s\n", jsonString)
 }
