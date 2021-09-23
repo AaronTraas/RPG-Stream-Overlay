f01d6b513c82d65a38e58fa72808d9ea55265166
Aaron Traas <aaron@traas.org>
2021-09-23 19:23:41 -0400

    go fmt

--- src/sheetservice.go
+++ src/sheetservice.go
@@ -171,10 +171,10 @@ func (app CharacterSheetServiceApp) LookupCharacter(charKey string) (*map[string
 	log.Printf("CACHE miss - Retrieving attributes for '%s'... ", charConfig.CharacterKey)
 	charMap := app.fetchCharacterAttributesFromSheetsApi(charConfig)
 	app.Cache.Set(charKey, charMap, cache.DefaultExpiration)
-	// FIXME - potential race condition here. I should probably manage cache expiry manually, and 
-	// trigger a goroutine locked behind a semaphor to update the cache in the background, and in 
-	// the meantime return the cached values. This way, we can never be making more than one query 
-	// at a time. 
+	// FIXME - potential race condition here. I should probably manage cache expiry manually, and
+	// trigger a goroutine locked behind a semaphor to update the cache in the background, and in
+	// the meantime return the cached values. This way, we can never be making more than one query
+	// at a time.
 
 	return charMap, true, false
 }
